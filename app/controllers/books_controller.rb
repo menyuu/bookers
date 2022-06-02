2f71bc962e093e2d93e00657a927a0d3c33ad38d
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-02 13:34:53 +0000

    editのcontrollerとviewの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to root_path
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -16,12 +16,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
-  
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
