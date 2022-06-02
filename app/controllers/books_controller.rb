1b378891ca329bf7bdd5da39c4e8f57e0baae454
menyuu <101458830+menyuu@users.noreply.github.com>
2022-06-02 13:02:38 +0000

    indexのcontrollerとviewの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,14 @@
 class BooksController < ApplicationController
+  
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to root_path
   end
 
   def show
@@ -7,4 +16,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
